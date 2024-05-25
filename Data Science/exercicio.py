68266d7ac004888f8bd08b003700cb13b6d6e893
EduardoDonini <145741177+EduardoDonini@users.noreply.github.com>
2024-05-25 14:28:11 -0300

    Add files via upload

---
+++ Data Science/exercicio.py
@@ -0,0 +1,16 @@
+# num = int(input("Digite um número:"))
+# print(num * num)
+
+# nome = str("Eduardo")
+# sobrenome = str("Donini")
+# print(f" {nome} {sobrenome}")
+
+# n1 = int(input("Digite um número:"))
+# n2 = int(input("Digite um número:"))
+# print(n1, n2)
+
+# palavra = str('PYTHON')
+# print(f'{palavra}1')
+
+# frase = input('Complete a frase - Eu sou ')
+# print(f"Eu sou {frase}")
---
+++ Data Science/listas.py
@@ -0,0 +1,35 @@
+# resultados = [10,20,60,80]
+# resultados.append (100)
+# print(resultados)
+
+# vendas = [150, 220, 180, 210, 190]
+# soma = sum(vendas)
+# print(soma)
+
+# precisoes = [0.85, 0.92, 0.78, 0.88, 0.90]
+# maximo = max(precisoes)
+# print(maximo)
+
+# dados = [120, 130, None, 140, 120, 150, None]
+# dados.remove (120 and None) 
+# print(dados)
+
+# dados_originais = [35, 40, 45, 50, 55]
+# copia = dados_originais.copy ()
+# print(copia)
+
+# amostras = [0.5, 0.6, 0.7]
+# adicionar = amostras.extend ([0.8, 0.9, 1.0])
+# print(amostras)
+
+# coluna = ["A", "B", "C", "A", "B", "D", "E"]
+# frequencia = coluna.count ("A")
+# print(frequencia)
+
+# registros = ["ID-001", "ID-002", "ID-003", "ID-004"]
+# posicao= registros.index ("ID-002")
+# print(posicao)
+
+# registros = ["ID-001", "ID-002", "ID-004", "ID-005"]
+# adc = registros.insert (2, "ID-003")
+# print(registros)

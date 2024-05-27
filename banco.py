a22b921164bcf21836efcb7e03bbf857afc3eb92
EduardoDonini <145741177+EduardoDonini@users.noreply.github.com>
2024-05-27 16:11:50 -0300

    banco.py

--- banco.py
+++ banco.py
@@ -2,7 +2,9 @@ print ("\n")
 print ("-----------Seja bem-vindo ao seu banco digital-----------")
 print ("\n")
 
-saldo = 1000
+saldo = int()
+limite = 500
+
 
 senha = [2,1,0]
 for n  in senha:
@@ -13,17 +15,25 @@ for n  in senha:
           print('Chances esgotadas, entre em contato com o banco') 
     else:
         print('Autorizado')
+        
         print ("\n")
         print ("Qual operação deseja realizar?")
-        op = input ("Saque, Depósito, Saldo:")
-        if op == "Saque":
+        op = input ("[1]-Saque [2]-Depósito [3]-Saldo:")
+        if op == "1":
             saque = int(input("Quanto deseja sacar?"))
-            print(f"Seu saque de {saque} foi autorizado. Seu saldo agora é de {saldo - saque}. ")
-        if op == "Depósito":
+            if saque > (limite):
+                print("Saque não autorizado.")
+            if saque > (saldo):
+                print("Saque não autorizado.")
+            else:
+                print(f"Seu saque de {saque} foi autorizado. Seu saldo agora é de {saldo - saque}. ")
+        if op == "2":
             deposito = int(input("Quanto deseja depositar?"))
             print(f"Seu depósito de {deposito} foi concluído. Seu saldo agora é de {saldo + deposito}. ")
-        if op == "Saldo":
+            saldo = saldo + deposito
+        if op == "3":
             print(f"Seu saldo é de {saldo}.")
+            
     while True:
         print("Deseja realizar outra operação?")
         quest = input("[1]-Sim  [2]-Não ")
@@ -32,14 +42,21 @@ for n  in senha:
             break
         if quest == "1":
             print ("Qual operação deseja realizar?")
-            op = input ("Saque, Depósito, Saldo:")
-        if op == "Saque":
+            op = input ("[1]-Saque [2]-Depósito [3]-Saldo:")
+        if op == "1":
             saque = int(input("Quanto deseja sacar?"))
-            print(f"Seu saque de {saque} foi autorizado. Seu saldo agora é de {saldo - saque}. ")
-        if op == "Depósito":
+            if saque > (limite):
+                print("Saque não autorizado.")
+            if saque > (saldo):
+                print("Saque não autorizado.")
+            else:
+                print(f"Seu saque de {saque} foi autorizado. Seu saldo agora é de {saldo - saque}. ")
+                saldo = saldo - deposito
+        if op == "2":
             deposito = int(input("Quanto deseja depositar?"))
             print(f"Seu depósito de {deposito} foi concluído. Seu saldo agora é de {saldo + deposito}. ")
-        if op == "Saldo":
+            saldo = saldo + deposito
+        if op == "3":
             print(f"Seu saldo é de {saldo}.")
           
     break
